4444d214dade4757f52c5b7e4a8f230a2e173707
VGFreak95 <towerterror00@gmail.com>
2023-07-27 11:53:40 -0400

    Added data extractor and use the el.json to make simple tables

---
+++ worlds/enderlilies/data/DataExtractor.py
@@ -0,0 +1,26 @@
+import json
+
+json_data = ""
+
+with open("data/el.json", "r") as json_file:
+    for line in json_file.readlines():
+        json_data += line
+
+ds_data = json.loads(json_data)
+
+alias = ds_data["item_alias"]
+
+spirits_table = alias["spirits"]
+abilities_table = alias["abilities"]
+relics_table = alias["relics"]
+
+macros_table = ds_data["macros"]
+
+extra_items_list = ds_data["extra_items"]
+
+locations_table = ds_data["locations"]
+
+nodes_alias_table = ds_data["nodes_alias"]
+
+nodes_table = ds_data["nodes"]
+
